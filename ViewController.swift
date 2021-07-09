e7f0bc5347a1bf2970d6804b649ebde5bbc16c5f
crus <romanghiet05@gmail.com>
2021-07-09 02:41:11 -0700

    core data implementation

--- ViewController.swift
+++ ViewController.swift
@@ -1,8 +1,4 @@
 //
-//  ViewController.swift
-//  DailyTask
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -17,11 +13,75 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
+       
+        // fetching data
+        fetchTask()
     }
     
+    //fecthing data from core data
+    func fetchTask() {
+        
+        //try! content.fetch(Tasks.fetchRequest()) or error check
+        do {
+            
+            self.dataLoad = try content.fetch(Tasks.fetchRequest())
+            //reload data to main thread
+            DispatchQueue.main.async {
+                self.table.reloadData()
+            }
+        }
+        catch{
+            //error check
+            print("error fething data")
+        }
+    }
     
-   
+    //add button
+    @IBAction func addBtn(_ sender: Any) {
+            //Alert Box
+            let alert   = UIAlertController(title: "new task", message: "enter your task", preferredStyle: .alert)
+            alert.addTextField()
+            
+            //alret save process
+            let saveBtn = UIAlertAction(title: "Save", style: .default , handler: {  (_) in
+                
+                //catch alert input data for creation entity
+                let alertText = alert.textFields![0]
+                
+                //check if enter an empty task content
+                if(alertText.text != ""){
+                    let newTask = Tasks(context: self.content)
+                    newTask.taskText = alertText.text
+                }
+                else{
+                    print("empty alert")
+                }
+                
+                //save data from alert box
+                do{
+                    try self.content.save()
+                }
+                catch {
+                    print("error fetching alert box")
+                }
+                
+                //update data
+                self.fetchTask()
+                
+            })
+            
+            //finish alert save process
+            alert.addAction(saveBtn)
+            
+            //cancel button
+            let  cancelBtn = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alert.addAction(cancelBtn)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            //for debugging
+            print("addBtn tapped")
+        }
+    
 }
 
