7977862491cec43f44a4be1ef5db7bface8238f6
crus <romanghiet05@gmail.com>
2021-07-11 02:52:07 -0700

    pickerView implementation

--- ColorFeature/ColorChangeBtn.swift
+++ ColorFeature/ColorChangeBtn.swift
@@ -4,14 +4,36 @@
 //
 
 import UIKit
+//Color Data
+var colorOption = ["TRUE RED","MEDIUM CRIMSON","WISTERIA PURPLE","SKY COLOR","ULTRAMARINE COLOR","PICTON BLUE",
+                   "INDIGO COLOR","NAVY BLUE COLOR","LIGHT BLUE COLOR","GREEN BAMBOO","SISKIN SPROUT YELLOW",
+                   "JUNGLE GREEN","OCEAN GREEN","JAPANESE TRIANDRA GRASS","RIPE LEMON","CREAM CAN"]
 
-extension ViewController{
-   
+extension ViewController: UIPickerViewDelegate ,UIPickerViewDataSource{
+    //PickerView Initialize
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colorOption.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colorOption[row]
+    }
     
     //Change Color Buttton Operations
     @IBAction func colorBtn(_ sender:Any){
         let colorAlert = UIAlertController(title: " ", message: "\n\n\n\n\n\n", preferredStyle: .actionSheet)
-
+        
+        //pickerView Implementatioon inside alert
+        let colorPicker = UIPickerView(frame: CGRect( x: 0, y: 0, width: 200, height: 300))
+        colorPicker.delegate = self
+        colorPicker.dataSource =  self
+        
+        //alert SubViews adds
+        colorAlert.view.addSubview(colorPicker)
+        
         //Control Actions
         let colorChange = UIAlertAction(title: "Change", style: .default ){ _ in }
         let cancel = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
